71f196f1f28f6230a7a0593fa3f4713e147a45c0
hashrith07 <hashrithchintanaga@gmail.com>
2026-03-25 00:30:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_BASE = "https://salary-prediction-m55r.onrender.com";
+const API_BASE = "https://Salary_Prediction-m55r.onrender.com";
 
 function spawnParticles() {
   const container = document.getElementById('particles');
